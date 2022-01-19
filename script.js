997b8dc8ea1fde2a65870f600bcfb89adcc56a1a
Joseph Manning <jgmanning3@yahoo.com>
2022-01-19 09:44:38 -0700

    Render data, images, and video for each individual date

--- script.js
+++ script.js
@@ -33,7 +33,35 @@ function displayData(data) {
   //   console.log({ title });
   //   console.log({ date });
   //   console.log({ image });
-  document.getElementById("title").textContent = data.map(data => data.title);
-  document.getElementById("date").textContent = data.map(data => data.date);
-//   document.getElementById("image").src = data.map(data => data.url);
+  console.log("length", data.length);
+  //   for (let i = 0; i < data.length; i++){
+  //       document.getElementById("title").textContent = data[i].title;
+  //       document.getElementById("date").textContent = data[i].date;
+
+  //   }
+  //   document.getElementById("image").src = data.map(data => data.url);
+
+//   <img src="${i.thumbnail_url}" alt="${i.explanation}"/>
+
+
+  let allImage = [];
+  for (let i of data) {
+    if (i.media_type === "video") {
+      allImage += `<div>
+        <h2>${i.title}</h2>
+        <p>${i.date}</p>
+        <iframe width="420" height="345" src="${i.url}">Video Not Available
+</iframe>
+        <button type="button">Like</button>
+        </div>`;
+    } else {
+      allImage += `<div>
+            <h2>${i.title}</h2>
+            <p>${i.date}</p>
+            <img src="${i.url}" alt="${i.explanation}"/>
+            <button type="button">Like</button>
+            </div>`;
+    }
+  }
+  document.getElementById("allImages").innerHTML = allImage;
 }
