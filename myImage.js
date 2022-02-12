152633b61e40282b0a8b4219da201512e7902320
Joseph Manning <jgmanning3@yahoo.com>
2022-02-11 17:50:44 -0700

    Fix bug where saved images not render in sequence

--- myImage.js
+++ myImage.js
@@ -3,17 +3,17 @@
 
 var API_KEY = "hAqRzYzMAFDKEnVTacuBgxtTayZqHDvb5UqazeiX";
 
-let likeImgArr = JSON.parse(localStorage.getItem("likeImgArr")) || [];
-console.log({ likeImgArr });
+let favImages = [];
 
 // Render saved images from local storage
-window.onload = function getFavData() {
-  let favImages = [];
-
-  for (i = 0; i < likeImgArr.length; i++) {
+window.onload = async function getFavData() {
+  const likeImgArr =
+    (await JSON.parse(localStorage.getItem("likeImgArr"))) || [];
+  console.log({ likeImgArr });
+  for (const likeImg of likeImgArr) {
     const api_url =
       `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&concept_tags=True&thumbs=True&date=` +
-      likeImgArr[i];
+      likeImg;
     console.log({ api_url });
     // console.log("API url", api_url);
     // const response = await fetch(url);
@@ -22,7 +22,7 @@ window.onload = function getFavData() {
     //     hideloader();
     // }
     // display(data);
-    fetch(api_url)
+    await fetch(api_url)
       .then((response) => {
         return response.json();
       })
@@ -32,25 +32,25 @@ window.onload = function getFavData() {
       });
     function displayFavImg(data) {
       // for (let i = 0; i < data.length; i++) {
-        if (data.media_type === "video") {
-          favImages.push(
-            `<div id="imgDesc">
+      if (data.media_type === "video") {
+        favImages.push(
+          `<div id="imgDesc">
                     <h2>${data.title}</h2>
                     <p>${data.date}</p>
                     <iframe width="420" height="345" src="${data.url}">Video Not Available</iframe>
                     <button type="button" class="btnClass" id="likeBtn"  >Unlike</button>
                     </div>`
-          );
-        } else {
-          favImages.push(
-            `<div id="imgDesc">
+        );
+      } else {
+        favImages.push(
+          `<div id="imgDesc">
                         <h2>${data.title}</h2>
                         <p>${data.date}</p>
                         <img src="${data.url}" alt="${data.explanation}"/>
                         <button type="button" class="btnClass" id="likeBtn"  >Unlike</button>
                         </div>`
-          );
-        }
+        );
+      }
       // }
       console.log({ favImages });
       document.querySelector(".myFavImages").innerHTML = favImages.join("");
